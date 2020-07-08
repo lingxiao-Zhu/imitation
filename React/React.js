27a4d058a62952bbc760dec79c1df99d93b8c7da
朱凌霄 <larry_zhu@foxmail.com>
2020-07-09 00:37:36 +0800

    chore: git 取消大小写忽略

---
+++ React/React.js
@@ -0,0 +1,38 @@
+import { useState, useEffect } from './ReactHooks';
+/**
+ *
+ * @param {*} type 节点类型，eg：div、h1，函数组件是函数本身
+ * @param {Object} props
+ * @param  {...any} children
+ */
+function createElement(type, props, ...children) {
+  return {
+    type,
+    props: {
+      ...props,
+      children: children.map((child) => (typeof child === 'object' ? child : createTextElement(child))),
+    },
+  };
+}
+
+/**
+ *
+ * @param {string} text
+ */
+function createTextElement(text) {
+  return {
+    type: 'TEXT_ELEMENT',
+    props: {
+      nodeValue: text,
+      children: [],
+    },
+  };
+}
+
+const React = {
+  createElement,
+};
+
+export { useState, useEffect };
+
+export default React;
