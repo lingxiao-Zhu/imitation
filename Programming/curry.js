25c5a05ff7dd07ccd10f479cac901f50dcfff3cb
朱凌霄 <larry_zhu@foxmail.com>
2021-02-04 23:51:43 +0800

    feat(programming): 完成 curry 化

--- Programming/curry.js
+++ Programming/curry.js
@@ -1,16 +1,22 @@
-// 无限次调用
-function curry(...args) {
-  const values = [args];
-  function _curry(...innerArgs) {
-    values.push(...innerArgs);
-    return _curry;
-  }
-
-  _curry.toString = function () {
-    return values.reduce((prev, next) => {
-      return prev + next;
-    }, 0);
+/**
+ * 柯里化，通过fn.number判断参数数量
+ * @param {function} fn
+ * @example
+ * function add(x,y,z){ return x+y+z }
+ * const curAdd = curry(add);
+ * curAdd(1,2,3)   === 6
+ * curAdd(1)(2)(3) === 6
+ * curAdd(1,2)(3)  === 6
+ */
+function curry(fn) {
+  const len = fn.length;
+  const allArgs = [];
+  const _innerFunc = (...args) => {
+    allArgs.push(...args);
+    if (allArgs.length >= len) {
+      return fn.apply(null, allArgs);
+    }
+    return _innerFunc;
   };
-
-  return _curry;
+  return _innerFunc;
 }
--- Programming/index.js
+++ Programming/index.js
@@ -1,34 +1,4 @@
 /**
- * 把接受多个参数的函数变换成接受一个单一参数（最初函数的第一个参数）的函数，并且返回接受余下的参数而且返回结果的新函数的技术。
- * @param {Function} fn
- * @example
- * function add(x,y,z){ return x+y+z }
- * const curAdd = curry(add);
- * curAdd(1,2,3)   === 6
- * curAdd(1)(2)(3) === 6
- * curAdd(1,2)(3)  === 6
- */
-function curry(fn) {
-  const len = fn.length;
-  const allArgs = [];
-  const innerFunc = function (...args) {
-    allArgs.push(...args);
-    if (allArgs.length < len) return innerFunc;
-    return fn.apply(this, allArgs);
-  };
-  return innerFunc;
-}
-
-/**
- * 惰性函数
- */
-function lazyFunc() {
-  const val = Date.now();
-  lazyFunc = () => val;
-  return lazyFunc();
-}
-
-/**
  * 防抖
  * @param {Function} fn
  * @param {Number} timeout
