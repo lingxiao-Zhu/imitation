b90c9da95bd70512245dbc275a1a3e0f3ac48895
LarryZhu <zhulingxiao@xigua.city>
2021-02-26 11:14:49 +0800

    feat(multirequest): 完成限流请求函数

--- Programming/scheduler.js
+++ Programming/scheduler.js
@@ -27,5 +27,37 @@ class Scheduler {
  * 要求最大并发数 maxNum
  * 每当有一个请求返回，就留下一个空位，可以增加新的请求
  * 所有请求完成后，结果按照 urls 里面的顺序依次打出
+ * @param {Array<string>} urls
+ * @param {number} maxNum
  */
-function request(urls, maxNum) {}
+function multiRequest(urls, maxNum) {
+  const res = [];
+  let i = 0; // urls下标
+  let resolve;
+  let promise = new Promise((r) => (resolve = r)); // 一直pending，等到所有url完成再resolve
+
+  const request = (url) => {
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        resolve(url);
+      }, 2000);
+    });
+  };
+
+  const start = () => {
+    if (!urls[i]) return resolve();
+    console.log('start fetch:', urls[i]);
+    const task = request(urls[i++]).finally(() => {
+      start();
+    });
+    res.push(task);
+  };
+
+  while (i < maxNum) {
+    start();
+  }
+
+  return promise.then(() => Promise.all(res));
+}
+
+multiRequest([1, 2, 3, 4, 5, 6], 2).then((res) => console.log(res));
