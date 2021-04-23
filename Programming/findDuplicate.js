5b7d6a1deecf3c30bc5dd72a108581a41d7c58c6
朱凌霄 <larry_zhu@foxmail.com>
2021-04-24 00:48:22 +0800

    feat: finish findDuplicate

--- Programming/findDuplicate.js
+++ Programming/findDuplicate.js
@@ -2,4 +2,21 @@
  *  找出数组中出现大于 times 次数的数
  * @param {number} times
  */
-Array.prototype.findDuplicate = function (times) {};
+Array.prototype.findDuplicate = function (times) {
+  if (!Array.isArray(this)) {
+    throw TypeError('findDuplicate must be used for array');
+  }
+
+  const obj = {};
+  const res = [];
+
+  for (let i of this) {
+    obj[i] = (obj[i] || 0) + 1;
+    if (obj[i] === times) {
+      res.push(i);
+    }
+  }
+  return res;
+};
+
+console.log([1, 2, 3, 4, 1, 2, 2, 2].findDuplicate(2));
