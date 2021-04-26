e32e93cdd19d6185eca096c0e6dbd7d2c7125aff
朱凌霄 <larry_zhu@foxmail.com>
2021-04-26 23:50:41 +0800

    feat: 完成 最长回文字符串

---
+++ Algorithm/longestPalindrome.js
@@ -0,0 +1,27 @@
+/**
+ * 回文字符串：中间扩散法
+ * @param {string} s
+ * @return {string}
+ */
+var longestPalindrome = function (s) {
+  if (s.length < 2) return s;
+
+  let res = '';
+
+  for (let i = 0; i < s.length; i++) {
+    hepler(i, i);
+    hepler(i, i + 1);
+  }
+
+  function hepler(m, n) {
+    while (s[m] === s[n] && m >= 0 && n < s.length) {
+      m--;
+      n++;
+    }
+    if (n - m - 1 > res.length) {
+      res = s.substring(m + 1, n);
+    }
+  }
+
+  return res;
+};
