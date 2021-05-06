23e00969585f5438bd0d7c5189a64c7257800e42
朱凌霄 <larry_zhu@foxmail.com>
2021-05-06 23:29:05 +0800

    feat: 完成 合并两个有序链表

---
+++ Algorithm/mergeTwoLists.js
@@ -0,0 +1,30 @@
+// Definition for singly-linked list.
+function ListNode(val, next) {
+  this.val = val === undefined ? 0 : val;
+  this.next = next === undefined ? null : next;
+}
+
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+var mergeTwoLists = function (l1, l2) {
+  const dummyHead = new ListNode(null);
+  let p = dummyHead;
+
+  while (l1 && l2) {
+    if (l1.val > l2.val) {
+      p.next = l1;
+      l1 = l1.next;
+    } else {
+      p.next = l2;
+      l2 = l2.next;
+    }
+    p = p.next;
+  }
+
+  p.next = l1 ? l1 : l2;
+
+  return dummyHead.next;
+};
