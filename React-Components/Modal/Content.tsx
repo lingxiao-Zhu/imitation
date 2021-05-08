642399fd559f727596f13f581596ecf18c90e366
lingxiao-Zhu <larry_zhu@foxmail.com>
2021-05-08 18:17:17 +0800

    feat(rc-components): 简化结构

--- React-Components/Modal/Content.tsx
+++ React-Components/Modal/Content.tsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from 'react';
-import Mask from './Mask';
 
 type IProps = {
   visible: boolean;
--- React-Components/Modal/Modal.tsx
+++ React-Components/Modal/Modal.tsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from 'react';
 import Portal from './Portal';
-import Mask from './Mask';
 import Content from './Content';
 
 type IProps = {
@@ -24,9 +23,10 @@ function Modal({ visible }: IProps) {
   }, [visible]);
 
   return (
+    // Portal 初始化后，就一直存在不被销毁
     <Portal visible={visible}>
       <div className="rc-modal-root">
-        <Mask visible={visible} />
+        {visible && <div className="rc-modal-mask"></div>}
         <div className="rc-modal-warp" style={{ display: !animatedVisible ? 'none' : null }}>
           <Content visible={visible} onVisibleChanged={onDialogVisibleChanged} />
         </div>
