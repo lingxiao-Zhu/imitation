a646f0d2e58118734fbfb1132dcb5e8496825af3
朱凌霄 <larry_zhu@foxmail.com>
2021-05-09 22:50:57 +0800

    docs: add some comments

---
+++ Algorithm/maxSubArray.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ * @description 剑指 Offer 42. 连续子数组的最大和
+ *
+ * 输入: nums = [-2,1,-3,4,-1,2,1,-5,4]
+ * 输出: 6
+ * 解释: 连续子数组 [4,-1,2,1] 的和最大，为 6。
+ *
+ * 要求时间复杂度为O(n)。
+ */
+var maxSubArray = function (nums) {};
