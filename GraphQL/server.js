1f8944756043604973cf33daf028c722cb313dd5
朱凌霄 <larry_zhu@foxmail.com>
2022-09-26 00:09:27 +0800

    feat: test query and mutation

--- GraphQL/server.js
+++ GraphQL/server.js
@@ -2,20 +2,62 @@ var express = require('express');
 var { graphqlHTTP } = require('express-graphql');
 var { buildSchema } = require('graphql');
 
+const users = [
+  { id: 1, name: 'John Doe', email: 'johndoe@gmail.com' },
+  { id: 2, name: 'Jane Doe', email: 'janedoe@gmail.com' },
+  { id: 3, name: 'Mike Doe', email: 'mikedoe@gmail.com' },
+];
+
 // Construct a schema, using GraphQL schema language
 var schema = buildSchema(`
+  input UserInput {
+    email: String!
+    name: String!
+  }
+
+  type User {
+    id: Int!
+    name: String!
+    email: String!
+  }
+
+  type Mutation {
+    createUser(input: UserInput!): User
+    updateUser(id: Int!, input: UserInput): User
+  }
+
   type Query {
-    hello: String,
-    age: Int
+    getUser(id: String): User
+    getUsers: [User]
   }
 `);
 
+const getUser = (args) => users.find((u) => u.id === args.id);
+
+const getUsers = () => users;
+
+const createUser = (args) => {
+  const user = {
+    id: users.length + 1,
+    ...args.input,
+  };
+  users.push(user);
+  return user;
+};
+
+const updateUser = (args) => {
+  const index = users.findIndex((u) => u.id === args.user.id);
+  const targetUser = users[index];
+  if (targetUser) users[index] = args.user;
+  return targetUser;
+};
+
 // The root provides a resolver function for each API endpoint
 var root = {
-  hello: () => {
-    return 'Hello world!';
-  },
-  age: () => 10,
+  getUser,
+  getUsers,
+  createUser,
+  updateUser,
 };
 
 var app = express();
