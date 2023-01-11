53808a3d377aa7e417f47740b0fb1cad09d7b6ea
朱凌霄 <larry_zhu@foxmail.com>
2023-01-12 00:23:16 +0800

    feat: 创建 wxml 节点

--- Taro2/wxml-to-react/tools/template/parser.ts
+++ Taro2/wxml-to-react/tools/template/parser.ts
@@ -1,12 +1,22 @@
+import { BaseNode, ElementNode, TextNode } from './nodes';
 import Tokenizer from './tokenizer';
+
+type RootNode = {
+  childNodes: BaseNode[];
+};
+
 export default class Parser {
   scanner: Tokenizer;
+  root: RootNode;
+  parent: RootNode | ElementNode;
+  node?: BaseNode;
 
   constructor(code: string) {
     this.scanner = new Tokenizer(code);
     this.root = {
       childNodes: [],
     };
+    this.parent = this.root;
   }
 
   static parse(code: string) {
